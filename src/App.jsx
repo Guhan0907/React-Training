534643ac2d1bd9a7def5e66eabd2e90401cd03f2
Guhan0907 <guhan.k@rently.com>
2025-06-19 10:14:01 +0530

    Paused added

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,3 @@
-// import React, { Component, PureComponent } from "react";
-// import Timer from "./components/Trial";
-// import ControlledInput from "./components/ControlledInput";
-// import UncontrolledInput from "./components/UnControlledInput";
-
-// class MyComponent extends PureComponent {
-//   // componentDidMount() {
-//   //   console.log("Component mounted");
-//   // }
-
-//   // componentWillUnmount() {
-//   //   console.log("Component will unmount");
-//   // }
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showTimer: true,
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div
-//         key={"uni"}
-//         style={{ height: "300px", width: "100%", background: "pink" }}
-//       >
-//         {" "}
-//         {this.state.showTimer && <Timer />}
-//         {/* <ControlledInput /> */}
-//         {/* <UncontrolledInput /> */}
-//       </div>
-//     );
-//   }
-// }
-
-// export default MyComponent;
-
-
 import React, { PureComponent } from "react";
 import Timer from "./components/Trial";
 import ControlledInput from "./components/ControlledInput";
@@ -50,11 +11,7 @@ class MyComponent extends PureComponent {
     };
   }
 
-  toggleTimer = () => {
-    this.setState((prev) => ({
-      showTimer : !prev.showTimer,
-    }))
-  }
+  toggleTimer = () => this.setState({showTimer : !this.state.showTimer})
 
   render() {
     return (
--- src/components/Trial.jsx
+++ src/components/Trial.jsx
@@ -1,33 +1,3 @@
-// import React , {Component} from "react";
-
-// class Timer extends React.Component {
-//   componentDidMount() {
-//     this.intervalId = setInterval(() => {
-//       console.log('Tick...');
-//     }, 1);
-
-//     this.timeoutId = setTimeout(() => {
-//       clearInterval(this.intervalId);
-//       console.log('Stopped after 10 seconds');
-//     }, 10000);
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.intervalId);
-//     console.log('Timer stopped and cleaned up');
-//   }
-
-//   render() {
-//     return <h1>Timer is running...</h1>;
-//   }
-// }
-
-// export default Timer;
-
-
-
-
-
 import React, { Component } from "react";
 
 class Timer extends Component {
@@ -37,6 +7,7 @@ class Timer extends Component {
       timer: 0,
       previousSpeed: props.speed,
       lap : [],
+      paused : false,
     };
     console.log("Constructor Completed");
   }
@@ -52,16 +23,18 @@ class Timer extends Component {
 
   componentDidMount() {
     console.log("Timer Started");
-
+    
     this.intervalId = setInterval(() => {
+
+      if (!this.state.paused){
       this.setState({
         timer: this.state.timer + 1,
       });
-
-      // if (this.state.timer >= 10){
-      //     clearInterval(this.intervalId);
-      // }
+    }
+     
     });
+
+
     console.log(this.intervalId, "this is it");
     this.timerOutId = setTimeout(() => {
       clearInterval(this.intervalId);
@@ -75,6 +48,7 @@ class Timer extends Component {
     console.log("Cleared due to Unmount");
   }
 
+  // lap button 
   handleLap = () => {
     let arr = [...this.state.lap]
     arr.push(this.state.timer)
@@ -83,16 +57,22 @@ class Timer extends Component {
     console.log("Lap Button =>> ", this.state.timer)
   }
 
+  // pause button
+  togglePause = () => {
+    this.setState({paused: !this.state.paused})
+    console.log("Timer has been paused");
+  }
+
   render() {
-    if (this.state.timer === 200) {
-      console.log("200");
-    }
+    // if (this.state.timer === 200) {
+    //   console.log("200");
+    // }
 
     return (
       <>
         <h2> Timings : {this.state.timer}</h2>
         <button onClick={this.handleLap}> Lap </button>
-
+        <button onClick={this.togglePause}> {this.state.paused ? "Resume" : "Pause"} </button>
         <div>
           <h3> Laps </h3>
           <ul>
