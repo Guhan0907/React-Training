c0328cc6a8ae5a63bb2ab72f13967c976d23e2b3
Guhan0907 <guhan.k@rently.com>
2025-06-27 17:48:19 +0530

    Typescript practise

---
+++ src/components/Practice.ts
@@ -0,0 +1,63 @@
+// // // Online Typescript Editor for free
+// // // Write, Edit and Run your Typescript code using TS Online Compiler
+
+// // var ans = "Hello";
+// // ans = "12"
+
+// // var kl : any = "summa"
+// // kl = 567
+
+// // const namek = ["summa"];
+// // namek.push(7)
+// // console.log(namek);
+
+// type CarNumber = number;
+// type CarName = String;
+// type CarRange = Boolean;
+// // Boolean CarRange = 
+
+// var naming: CarNumber = 57;
+// // naming: CarNumber = "Hello;
+// naming = 7890;
+// console.log(naming);
+
+// type Car = {
+//     year : CarYear;
+// }
+
+
+// interface tria {
+//     height : number,
+//     widht : number
+// }
+
+// const area: tria = {
+//     height : 20,
+//     widht : 20
+// };
+
+// console.log(area.height * area.widht)
+
+// function multiply(a: number , b: number): number {
+//     return a+b;    
+// }
+
+// console.log(multiply(7,9))
+
+// var assn = 12;
+// var val = assn as unknown as String;
+// console.log(val);
+
+let value = "hello";
+let lengh = (<string>value).length;
+console.log(lengh)
+
+
+ 
+ 
+
+
+
+
+
+
